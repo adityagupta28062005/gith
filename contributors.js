77f8e89ffaebcbc605dcd32b7c071c8fe780f187
Aditya_Gupta <aditya.gupta.ece23@itbhu.ac.in>
2023-10-01 18:14:44 +0530

    fresher namecard

---
+++ contributors.js
@@ -0,0 +1,37 @@
+const CONTRIBUTERS = [
+    {
+        handle_name: 'cops_iitbhu',
+        name: "First Card",
+        description: "Hello everyone, this is the first card. Go ahead, add your own!",
+        github: "https://github.com/COPS-IITBHU",
+    },
+    // Start adding your data below
+    {
+        handle_name: 'Varun-Kolanu',
+        name: "Varun Kolanu",
+        description: "Hello everyone, myself Varun from CSE Part 2",
+        github: "https://github.com/Varun-Kolanu"
+    },
+    {
+        handle_name: 'darshi1337',
+        name: "Priyadarshi Anupam",
+        description: "Hello everyone, myself Priyadarshi from Mechanical Part 2",
+        github: "https://github.com/darshi1337"
+    },
+    {
+        handle_name: 'shivansh-bhatnagar18',
+        name: "Shivansh Bhatnagar",
+        description: "Hello everyone, myself Shivansh from MnC Part 2",
+        github: "https://github.com/shivansh-bhatnagar18"
+    },
+     {
+        handle_name: 'adityagupta28062005',
+        name: "Aditya Gupta",
+        description: "Hello everyone, myself Aditya from ECE Part 1",
+        github: "https://github.com/adityagupta28062005"
+    },
+
+    
+];
+
+export default CONTRIBUTERS;
